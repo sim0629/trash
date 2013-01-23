7d132d49dc4dacb98f55920e6e5bcd5149813a77
sgm <sim0629@gmail.com>
2013-01-23 17:42:16 +0900

    init

---
+++ public_html/index.php
@@ -0,0 +1,29 @@
+<?php
+    $dday = (int)((mktime(0, 0, 0, 4, 21, 2014) - time()) / 60 / 60 / 24) + 1;
+    $type = $_GET['type'];
+    if($type == 'xml') {
+        $xml = '<?xml version="1.0" encoding="utf-8" ?>';
+        $xml .= "<dday>{$dday}</dday>";
+        header('Content-type', 'application/xml; charset=utf-8');
+        echo $xml;
+    }else if($type == 'json') {
+        $json = '{ "dday" : '.$dday.' }';
+        header('Content-type', 'application/json; charset=utf-8');
+        echo $json;
+    }else {
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <title>D-Day</title>
+    </head>
+    <body>
+        D-<?=$dday?>
+
+    </body>
+</html>
+<?php
+    }
+?>
