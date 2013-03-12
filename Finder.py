778bab1d890e5f156f1b7e3b9fcf986b02fc7f48
sgm <sim0629@gmail.com>
2013-03-12 16:47:54 +0900

    Add parse method

--- Finder.py
+++ Finder.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 
-import sys
+import BeautifulSoup
 import Queue
+import sys
+import urllib
 
 class Finder:
     def __init__(self, rival_ids):
         if not rival_ids:
             rival_ids = ["57710029431862"]
+        self._url_format = "http://p.eagate.573.jp/game/jubeat/saucer/s/playdata/history.html?rival_id=%s&page=%d"
         self._q = Queue.Queue()
         self._d = {}
         for rival_id in rival_ids:
@@ -18,8 +21,15 @@ class Finder:
         self._q.put(rival_id)
         self._d[rival_id] = True
 
+    def _parse(self, data):
+        soup = BeautifulSoup.BeautifulSoup(data)
+        print soup.prettify()
+
     def _crawl(self, rival_id):
-        return
+        for page_num in [1, 2, 3]:
+            url = self._url_format % (rival_id, page_num)
+            data = urllib.urlopen(url)
+            self._parse(data)
 
     def run(self):
         while not self._q.empty():
