de6d3b6098e11822c241a7ac450205f88ede6642
sgm <sim0629@gmail.com>
2013-04-08 15:43:17 +0900

    init

---
+++ snake.c
@@ -0,0 +1,26 @@
+#define w(e) && a[e] == c) q = q > g(e) ? q : g(e);
+
+a[101], n, x = 3, j, l, c, q, i;
+
+g(int i) {
+    c = a[i] + 1;
+    q = 0;
+    if(i >= x w(i - x)
+    if(i + x < n w(i + x)
+    if(i % x > 0 w(i - 1)
+    if(i % x < x - 1 w(i + 1)
+    return q + 1;
+}
+
+main() {
+    for(; ~scanf("%d", a + n); n++);
+    for(; x * x != n; x++);
+    
+    for(i = 0; i < n; i++)
+        if(g(i) > g(j)) j = i;
+
+    for(i = 0; i < g(j); i++)
+        printf("%d ", a[j] + i);
+
+    return 0;
+}
