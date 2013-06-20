25248ff2bbb8c56dd566c8abea4f2369a66292c3
gmsim <gmsim@innotive.com>
2013-06-20 16:12:45 +0900

    Add track.sh

---
+++ track.sh
@@ -0,0 +1,13 @@
+# File List Tracker
+
+git init
+git config user.name "track"
+git config user.email "track@localhost"
+
+while :
+do
+    git status > status.log
+    git add status.log
+    git commit -m "status" -uno
+    sleep 10
+done
