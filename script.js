c2f40ce5d8b9476938793fb32b47c8f15aa74d49
sgm <sim0629@gmail.com>
2014-07-29 16:49:22 +0900

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿/* sugang */
+
+var s = document.createElement("script");
+s.innerHTML =
+"var form = document.getElementById('CA102') || document.getElementById('CA202');" +
+"form && (form.onsubmit = function() {" +
+"	fnCourseApproveCheck();" +
+"	return false;" +
+"});" +
+"var fc_os = document.getElementsByClassName('fc_o'), fc_o;" +
+"fc_os && fc_os.length >= 3 && (fc_o = fc_os[2]);" +
+"var n = parseInt(fc_o.innerHTML);" +
+"$('.tbl_basic tbody tr').eq(n * 2).find('input').eq(0).click();"
+;
+s.onload = function() {
+	this.parentNode.removeChild(this);
+};
+(document.head || document.documentElement).appendChild(s);
