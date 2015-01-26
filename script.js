5f586d029d5270f24be4c70b7a752ac99d79631b
sgm <sim0629@gmail.com>
2015-01-26 16:43:09 +0900

    dos2unix, tab2spaces

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var s = document.createElement("script");
 s.innerHTML =
 "var form = document.getElementById('CA102') || document.getElementById('CA202');" +
 "form && (form.onsubmit = function() {" +
-"	fnCourseApproveCheck();" +
-"	return false;" +
+"  fnCourseApproveCheck();" +
+"  return false;" +
 "});" +
 "var fc_os = document.getElementsByClassName('fc_o'), fc_o;" +
 "fc_os && fc_os.length >= 3 && (fc_o = fc_os[2]);" +
@@ -14,6 +14,6 @@ s.innerHTML =
 "$('.tbl_basic tbody input[type=checkbox]').eq(m[n]).click();"
 ;
 s.onload = function() {
-	this.parentNode.removeChild(this);
+  this.parentNode.removeChild(this);
 };
 (document.head || document.documentElement).appendChild(s);
