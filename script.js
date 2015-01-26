00c68f2c28855ac08e827d4844856f3b6a476a38
sgm <sim0629@gmail.com>
2015-01-26 17:15:09 +0900

    refactor injecting the script

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 ﻿/* sugang */
 
+var inject = function() {
+  var form = document.getElementById('CA102') || document.getElementById('CA202');
+  form && (form.onsubmit = function() {
+    fnCourseApproveCheck();
+    return false;
+  });
+  var fc_os = document.getElementsByClassName('fc_o'), fc_o;
+  fc_os && fc_os.length >= 3 && (fc_o = fc_os[2]);
+  var n = parseInt(fc_o.innerHTML);
+  var m = [1, 5, 6, 2, 3, 0, 4];
+  $('.tbl_basic tbody input[type=checkbox]').eq(m[n]).click();
+};
+
 var s = document.createElement("script");
-s.innerHTML =
-"var form = document.getElementById('CA102') || document.getElementById('CA202');" +
-"form && (form.onsubmit = function() {" +
-"  fnCourseApproveCheck();" +
-"  return false;" +
-"});" +
-"var fc_os = document.getElementsByClassName('fc_o'), fc_o;" +
-"fc_os && fc_os.length >= 3 && (fc_o = fc_os[2]);" +
-"var n = parseInt(fc_o.innerHTML);" +
-"var m = [1, 5, 6, 2, 3, 0, 4];" +
-"$('.tbl_basic tbody input[type=checkbox]').eq(m[n]).click();"
-;
+s.innerHTML = "var inject = " + inject + "; inject();";
 s.onload = function() {
   this.parentNode.removeChild(this);
 };
